0e547dd1fe4a00b85499e57a4ab87ddf076e1a47
Jeffrey S <jeffrey@accentinteractive.nl>
2026-02-16 01:30:28 +0100

    Add notes for step 4

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,5 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+// Run the price calculator daily at the end of the day
+Schedule::command('app:calculate-daily-costs')->dailyAt('23:59');
