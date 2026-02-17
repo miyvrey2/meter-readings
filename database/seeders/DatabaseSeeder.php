0c6cf626aa34620f67815d79e2d4276ecf3080d2
Jeffrey S <jeffrey@accentinteractive.nl>
2026-02-17 15:03:27 +0100

    Renamed 'ean' to 'connection' & 'code' to 'ean_code' in seeder, changed the 'Sequence' to regular arrays to speed up the seeding process

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,11 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\DailyCosts;
-use App\Models\Ean;
+use App\Models\Connection;
 use App\Models\MeterReadings;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,11 +18,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $eans = Ean::factory(10)->create();
+        // Create random connections
+        $connections = Connection::factory(100)->create();
 
         $amount_of_days = 28;
+        $amount_of_inserts_per_day = 96;
 
-        foreach ($eans as $ean) {
+        foreach ($connections as $connection) {
 
             // Set a base for the current kwh
             $kwh_total = fake()->randomFloat(2, 1000, 2000);
@@ -38,25 +39,27 @@ class DatabaseSeeder extends Seeder
                 }
 
                 // Create meter_readings for a day, add in sequence (15 min) kwh and update timestamp
-                MeterReadings::factory()
-                         ->count(24 * 4)
-                         ->sequence(function (Sequence $sequence) use ($kwh_total, $kwh_increase, $amount_of_days, $i) {
-                             return [
-                                 'kwh_total' => (($kwh_total + $kwh_increase * $i) + $sequence->index * ($kwh_increase / ($i * 24 * 4))),
-                                 'timestamp' => now()->subDays($amount_of_days)->startOfDay()->addDays($i)->addMinutes($sequence->index * 15)
-                             ];
-                         })
-                         ->recycle($ean)
-                         ->create();
+                $meterRows = [];
+
+                for ($j = 0; $j < $amount_of_inserts_per_day; $j++) {
+                    $meterRows[] = [
+                        'ean_code' => $connection->ean_code,
+                        'kwh_total' => (($kwh_total + $kwh_increase * $i) + $j * ($kwh_increase / ($i * $amount_of_inserts_per_day))),
+                        'timestamp' => now()->subDays($amount_of_days)
+                                            ->startOfDay()
+                                            ->addDays($i)
+                                            ->addMinutes($j * (1440 / $amount_of_inserts_per_day)),
+                    ];
+                }
+                MeterReadings::insert($meterRows);
 
                 // Calculate and insert the dailyCosts
-                DailyCosts::factory(1)->recycle($ean)->create(
-                    [
-                        'timestamp' => now()->subDays($amount_of_days)->endOfDay()->addDays($i),
-                        'kwh_used' => $kwh_increase,
-                        'cost_in_euro' => $ean->cost_per_kwh_in_euro * $kwh_increase,
-                    ]
-                );
+                DailyCosts::insert([
+                    'ean_code' => $connection->ean_code,
+                    'timestamp' => now()->subDays($amount_of_days)->endOfDay()->addDays($i),
+                    'kwh_used' => $kwh_increase,
+                    'cost_in_euro' => $connection->cost_per_kwh_in_euro * $kwh_increase,
+                ]);
             }
         }
 
